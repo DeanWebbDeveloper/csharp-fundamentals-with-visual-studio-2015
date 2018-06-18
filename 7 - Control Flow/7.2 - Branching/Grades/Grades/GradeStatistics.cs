253872ae37231d57b0d789ea56765bf5730979da
deanwebbdeveloper <dean@deanwebbdeveloper.com>
2018-06-18 21:20:19 +0100

    Chapters 6 - 7
    Chapter 6: Methods, Fields, Events, and Properties completed
    
    Chapter 7 : Control Flow in progress, on 7.7: Handling

---
+++ 7 - Control Flow/7.2 - Branching/Grades/Grades/GradeStatistics.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    public class GradeStatistics
+    {
+        public GradeStatistics()
+        {
+            HighestGrade = 0;
+            LowestGrade = float.MaxValue;
+        }
+
+        public string LetterGrade
+        {
+            get
+            {
+                string result;
+                if (Math.Round(AverageGrade) >= 90)
+                {
+                    result = "A";
+                }
+                else if (Math.Round(AverageGrade) >= 80)
+                {
+                    result = "B";
+                }
+                else if (Math.Round(AverageGrade) >= 70)
+                {
+                    result = "C";
+                }
+                else if (Math.Round(AverageGrade) >= 60)
+                {
+                    result = "D";
+                }
+                else
+                {
+                    result = "F";
+                }
+                return result;
+            }
+        }
+
+        public float AverageGrade;
+        public float HighestGrade;
+        public float LowestGrade;
+    }
+}
