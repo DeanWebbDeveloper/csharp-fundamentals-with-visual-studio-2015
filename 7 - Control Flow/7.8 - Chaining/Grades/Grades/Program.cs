13187ddad3f888177d73f8e0440ec17ea2cfa6e7
deanwebbdeveloper <dean@deanwebbdeveloper.com>
2018-06-19 20:20:17 +0100

    Chapters 7-8
    Chapter 7 completed
    
    Chapter 8 started - 8.3 - Inheritance

---
+++ 7 - Control Flow/7.8 - Chaining/Grades/Grades/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook();
+
+
+
+            try
+            {
+                Console.WriteLine("Enter a name");
+                book.Name = Console.ReadLine();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong!");
+            }
+
+
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+            book.WriteGrades(Console.Out);
+
+            GradeStatistics stats = book.ComputeStatistics();
+            WriteResult("Average", stats.AverageGrade);
+            WriteResult("Highest", stats.HighestGrade);
+            WriteResult("Lowest", stats.LowestGrade);
+            WriteResult(stats.Description, stats.LetterGrade);
+        }
+
+        static void WriteResult(string description, string result)
+        {
+            Console.WriteLine($"{description}: {result}");
+        }
+
+
+        static void WriteResult(string description, float result)
+        {
+            Console.WriteLine($"{description}: {result:F2}");
+        }
+    }
+}
