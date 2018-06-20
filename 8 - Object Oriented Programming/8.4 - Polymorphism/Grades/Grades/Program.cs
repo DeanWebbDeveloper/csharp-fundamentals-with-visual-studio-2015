b1484c9c324169fe252bd31abc445ed445b5c9d9
deanwebbdeveloper <dean@deanwebbdeveloper.com>
2018-06-20 21:29:33 +0100

    Chapter 8
    Finishing Chapter 8
    
    Course complete

---
+++ 8 - Object Oriented Programming/8.4 - Polymorphism/Grades/Grades/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook book = CreateGradeBook();
+
+            //GetBookName(book);
+            AddGrades(book);
+            SaveGrades(book);
+            WriteResults(book);
+        }
+
+        private static GradeBook CreateGradeBook()
+        {
+            return new ThrowAwayGradeBook();
+        }
+
+        static void WriteResult(string description, string result)
+        {
+            Console.WriteLine($"{description}: {result}");
+        }
+
+        static void WriteResult(string description, float result)
+        {
+            Console.WriteLine($"{description}: {result:F2}");
+        }
+
+        private static void GetBookName(GradeBook book)
+        {
+            try
+            {
+                Console.WriteLine("Enter a name");
+                book.Name = Console.ReadLine();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong!");
+            }
+        }
+
+        private static void AddGrades(GradeBook book)
+        {
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+        }
+
+        private static void SaveGrades(GradeBook book)
+        {
+            using (StreamWriter outputFile = File.CreateText("grades.txt"))
+            {
+                book.WriteGrades(outputFile);
+            }
+        }
+
+        private static void WriteResults(GradeBook book)
+        {
+            GradeStatistics stats = book.ComputeStatistics();
+            WriteResult("Average", stats.AverageGrade);
+            WriteResult("Highest", stats.HighestGrade);
+            WriteResult("Lowest", stats.LowestGrade);
+            WriteResult(stats.Description, stats.LetterGrade);
+        }
+    }
+}
---
+++ 8 - Object Oriented Programming/8.5 - Abstract Classes/Grades/Grades.Tests/GradeBookTests.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Grades;
+
+namespace Grades.Tests
+{
+    [TestClass]
+    public class GradeBookTests
+    {
+        [TestMethod]
+        public void ComputesHighestGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(10);
+            book.AddGrade(90);
+
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual(90, result.HighestGrade);
+        }
+
+        [TestMethod]
+        public void ComputesLowestGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(10);
+            book.AddGrade(90);
+
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual(10, result.LowestGrade);
+        }
+
+        [TestMethod]
+        public void ComputesAverageGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual(85.16, result.AverageGrade, 0.01);
+        }
+
+        [TestMethod]
+        public void ComputesLetterGradeA()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(95);
+
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual("A", result.LetterGrade);
+        }
+
+        [TestMethod]
+        public void ComputesLetterGradeB()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(81);
+            book.AddGrade(84);
+            book.AddGrade(88);
+
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual("B", result.LetterGrade);
+        }
+
+        [TestMethod]
+        public void ComputesLetterGradeC()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(81.4f);
+            book.AddGrade(71);
+            book.AddGrade(73);
+
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual("C", result.LetterGrade);
+        }
+
+        [TestMethod]
+        public void ComputesLetterGradeD()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(60);
+            book.AddGrade(60);
+            book.AddGrade(60);
+
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual("D", result.LetterGrade);
+        }
+
+        [TestMethod]
+        public void ComputesLetterGradeF()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(42);
+            book.AddGrade(25);
+            book.AddGrade(0);
+
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual("F", result.LetterGrade);
+        }
+    }
+}
